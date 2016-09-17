47fc1d2f1e482c4a719c31bcd90d7761d36f56b5
gryphius <oli@wgwh.ch>
2016-09-17 08:50:10 +0200

    more initial stuff

--- dnssecscore_flaskbackend/backend.py
+++ dnssecscore_flaskbackend/backend.py
@@ -1,14 +1,73 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+from dnssecchecks import all_tests, DNSInfoBroker, TESTRESULTTYPE_ERROR, TESTRESULTTYPE_SECURE, RESULTTYPE_BAD
+import time
 
 @app.route("/testdomain")
 def testdomain():
     return "Hello World!"
 
-def checkdomain(domainname):
-    testresult=dict()
 
-    return flask.jsonify(testresult)
+@app.route('/d/<domainname>')
+def checkdomain(domainname):
+    testrun = DNSSECTest(domainname)
+    testrun.run_tests()
+
+    return jsonify(testrun.result_as_dict())
+
+
+
+class DNSSECTest(object):
+    def __init__(self, domain):
+        self.testresults=[]
+        self.domain = domain
+        self.broker = DNSInfoBroker(domain)
+        self.result_type = None
+        self.startdate = int(time.time())
+
+    def run_tests(self):
+        for testclass in all_tests:
+            testinstance = testclass(self.broker)
+            if not testinstance.do_we_have_what_we_need():
+                continue
+
+                testinstance.run_test()
+
+            # abort tests
+            if testinstance.shortcircuit!=None:
+                self.result_type = testinstance.shortcircuit
+                break
+
+            self.testresults.append(testinstance)
+
+
+    def result_as_dict(self):
+        resultdict = {
+            "startdate": self.startdate,
+            "domain": self.domain,
+            "total_tests": len(all_tests),
+            "tests":[],
+        }
+
+        score = 100
+        score_subtract_per_test = 100.0/(len(all_tests))
+
+        for result in self.testresults:
+            if result.resulttype == RESULTTYPE_BAD:
+                score -= (score_subtract_per_test * result_weight )
+            pass
+
+        if self.result_type == None:
+            self.result_type = TESTRESULTTYPE_SECURE
+
+
+        resultdict["result"] = self.result_type
+
+
+        return resultdict
+
+
+
 
 
 if __name__ == "__main__":
--- dnssecscore_flaskbackend/dnssecchecks.py
+++ dnssecscore_flaskbackend/dnssecchecks.py
@@ -1,9 +1,26 @@
 NXDOMAIN = None
 
+
+RESULTTYPE_UNKNOWN = "unknown"
+RESULTTYPE_GOOD = "good"
+RESULTTYPE_BAD = "bad"
+RESULTTYPE_NEUTRAL = "neutral"
+
+TESTRESULTTYPE_TRUSTISSUE = "T"
+TESTRESULTTYPE_INSECURE = "I"
+TESTRESULTTYPE_VALIDATIONFAILURE = "F"
+TESTRESULTTYPE_SECURE = "S"
+TESTRESULTTYPE_ERROR = "E"
+
+
 class DNSInfoBroker(object):
     def __init__(self, domain):
-        #TODO: load the domain info here
         self.domaininfo={}
+        self._load_info()
+        self.domain = domain
+
+    def _load_info(self):
+        pass
 
     def have_completed(self, rtype):
         return rtype in self.domaininfo
@@ -14,20 +31,61 @@ class DNSInfoBroker(object):
     def get_records(self, rtype):
         return self.domaininfo[rtype]
 
-class CheckBase(object):
+class TestBase(object):
     def __init__(self,broker):
         self.name="test name"
         self.description="test description"
-        self.broker = brokder
+        self.broker = broker
+        self.resulttype = RESULTTYPE_UNKNOWN
+
+        self.shortcircuit = None # set a TESTRESULTTYPE_... to abort all tests
+
+        self.result_weight = 1 # how important is this test. 0 for "informative only"
+        self.result_messages = []
+
+
 
     def do_we_have_what_we_need(self):
         #ask the broker if the records have been completed
-        return True
+        return False
 
-
-class CheckAreWeSigned(CheckBase):
-    def __init__(self):
+    def run_test(self):
         pass
 
 
-all_checks=[]
+class DummyInfo(TestBase):
+    def do_we_have_what_we_need(self):
+        return True
+
+    def run_test(self):
+        self.resulttype = RESULTTYPE_NEUTRAL
+        self.result_weight  = 0
+        self.result_messages = ["This is just a dummy info message to see if things are working. ", ]
+
+
+class DummyGood(TestBase):
+    def do_we_have_what_we_need(self):
+        return True
+
+    def run_test(self):
+        self.resulttype = RESULTTYPE_GOOD
+        self.result_weight  = 1
+        self.result_messages = ["This is just a dummy success result. That mean's you've passed... YAY!", ]
+
+class DummyBad(TestBase):
+    def do_we_have_what_we_need(self):
+        return True
+
+    def run_test(self):
+        self.resulttype = RESULTTYPE_BAAD
+        self.result_weight  = 1
+        self.result_messages = ["This is just a dummy fail result. Sorry to say you've failed this one... BOOOH!", ]
+
+
+class CheckAreWeSigned(TestBase):
+    def __init__(self, broker):
+        super(TestBase, self).__init__(broker)
+
+
+
+all_tests=[DummyInfo, DummyGood, DummyBad ]
