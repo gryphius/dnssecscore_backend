dadab8d6d5b1187e5d47ae43fe62343ebe723b67
Oli Schacher <oli@wgwh.ch>
2016-09-17 15:59:28 +0200

    Multithreaded.

--- dnssecscore_flaskbackend/all_the_shiet.py
+++ dnssecscore_flaskbackend/all_the_shiet.py
@@ -5,42 +5,21 @@ import dns.flags
 import base64
 import time
 import pprint
+import multiprocessing.pool
 
-def all_the_shiet(domain, dns_server = "8.8.8.8", output = False):
-
-    domain = dns.name.from_text(domain)
-    if not domain.is_absolute():
-        domain = domain.concatenate(dns.name.root)
-
-    # DNSKEY SOA DS RRSIG
-    # dnskey, algorithm, flags, protocol, ttl
-    # soa, expiry, ttl
-    # ds, digest type, algorithm, keytag, ttl
-    # rrsig, algorithm, start time, end time, key tag, ttl
-    # RRSIG
+def dnskey_shiet(domain, dns_server = "8.8.8.8", output = False):
     dnskey_request = dns.message.make_query(domain, dns.rdatatype.DNSKEY)
     dnskey_request.want_dnssec()
-    soa_request = dns.message.make_query(domain, dns.rdatatype.SOA)
-    soa_request.want_dnssec()
-    ds_request = dns.message.make_query(domain, dns.rdatatype.DS)
-    ds_request.want_dnssec()
-
     dnskey_answers = dns.query.udp(dnskey_request, dns_server).answer
-    soa_answers = dns.query.udp(soa_request, dns_server).answer
-    ds_answers = dns.query.udp(ds_request, dns_server).answer
-
-    response = dict()
-    response['DNSKEY'] = dict()
-    response['SOA'] = dict()
-    response['DS'] = dict()
 
     if output:
-        print "DNSKEY:"
+        print "DNSKEY"
         print "============================"
+    response = dict()
     if len(dnskey_answers) >= 1:
-        response['DNSKEY']['RR'] = []
+        response['RR'] = []
         for rdata in dnskey_answers[0]:
-            response['DNSKEY']['RR'].append({
+            response['RR'].append({
                 "algorithm" : rdata.algorithm,
                 "flags" : rdata.flags,
                 "key" : base64.b64encode(rdata.key),
@@ -55,9 +34,9 @@ def all_the_shiet(domain, dns_server = "8.8.8.8", output = False):
                 print "  TTL: ", dnskey_answers[0].ttl
                 print "----------------------------"
     if len(dnskey_answers) >= 2:
-        response['DNSKEY']['RRSIG'] = []
+        response['RRSIG'] = []
         for rdata in dnskey_answers[1]:
-            response['DNSKEY']['RRSIG'].append({
+            response['RRSIG'].append({
                 "algorithm" : rdata.algorithm,
                 "inception" : rdata.inception,
                 "key_gat" : rdata.key_tag,
@@ -72,29 +51,37 @@ def all_the_shiet(domain, dns_server = "8.8.8.8", output = False):
                 print "  TTL: ", dnskey_answers[1].ttl
                 print "----------------------------"
 
+    return response
+
+def soa_shiet(domain, dns_server = "8.8.8.8", output = False):
+    soa_request = dns.message.make_query(domain, dns.rdatatype.SOA)
+    soa_request.want_dnssec()
+    soa_answers = dns.query.udp(soa_request, dns_server).answer
+
     if output:
-        print "SOA:"
+        print "SOA"
         print "============================"
+    response = dict()
     if len(soa_answers) >= 1:
-        response['SOA']['RR'] = []
+        response['RR'] = []
         for rdata in soa_answers[0]:
-            response['SOA']['RR'].append({
+            response['RR'].append({
                 "expire" : rdata.expire,
-                "ttl" : dnskey_answers[0].ttl
+                "ttl" : soa_answers[0].ttl
             })
             if output:
                 print "  Expire: ", rdata.expire
                 print "  TTL: ", soa_answers[0].ttl
                 print "----------------------------"
     if len(soa_answers) >= 2:
-        response['SOA']['RRSIG'] = []
+        response['RRSIG'] = []
         for rdata in soa_answers[1]:
-            response['SOA']['RRSIG'].append({
+            response['RRSIG'].append({
                 "algorithm" : rdata.algorithm,
                 "inception" : rdata.inception,
                 "key_gat" : rdata.key_tag,
                 "expiration" : rdata.expiration,
-                "ttl" : dnskey_answers[1].ttl
+                "ttl" : soa_answers[1].ttl
             })
             if output:
                 print "  Algorithm: ", rdata.algorithm
@@ -104,17 +91,25 @@ def all_the_shiet(domain, dns_server = "8.8.8.8", output = False):
                 print "  TTL: ", soa_answers[1].ttl
                 print "----------------------------"
 
+    return response
+
+def ds_shiet(domain, dns_server = "8.8.8.8", output = False):
+    ds_request = dns.message.make_query(domain, dns.rdatatype.DS)
+    ds_request.want_dnssec()
+    ds_answers = dns.query.udp(ds_request, dns_server).answer
+
     if output:
-        print "DS:"
-        print "============"
+        print "DS"
+        print "============================"
+    response = dict()
     if len(ds_answers) >= 1:
-        response['DS']['RR'] = []
+        response['RR'] = []
         for rdata in ds_answers[0]:
-            response['DS']['RR'].append({
+            response['RR'].append({
                 "algorithm" : rdata.algorithm,
                 "digest_type" : rdata.digest_type,
                 "key_tag" : rdata.key_tag,
-                "ttl" : dnskey_answers[0].ttl
+                "ttl" : ds_answers[0].ttl
             })
             if output:
                 print "  Algorithm: ", rdata.algorithm
@@ -123,14 +118,14 @@ def all_the_shiet(domain, dns_server = "8.8.8.8", output = False):
                 print "  TTL: ", ds_answers[0].ttl
                 print "----------------------------"
     if len(ds_answers) >= 2:
-        response['DS']['RRSIG'] = []
+        response['RRSIG'] = []
         for rdata in ds_answers[1]:
-            response['DS']['RRSIG'].append({
+            response['RRSIG'].append({
                 "algorithm" : rdata.algorithm,
                 "inception" : rdata.inception,
                 "key_gat" : rdata.key_tag,
                 "expiration" : rdata.expiration,
-                "ttl" : dnskey_answers[1].ttl
+                "ttl" : ds_answers[1].ttl
             })
             if output:
                 print "  Algorithm: ", rdata.algorithm
@@ -139,6 +134,25 @@ def all_the_shiet(domain, dns_server = "8.8.8.8", output = False):
                 print "  Expiration: ", rdata.expiration
                 print "  TTL: ", ds_answers[1].ttl
                 print "----------------------------"
+
+    return response
+
+
+
+def all_the_shiet(domain, dns_server = "8.8.8.8", output = False):
+    domain = dns.name.from_text(domain)
+    if not domain.is_absolute():
+        domain = domain.concatenate(dns.name.root)
+
+    pool = multiprocessing.pool.ThreadPool(processes=1)
+    dnskey_result = pool.apply_async(dnskey_shiet, (domain, dns_server, output))
+    soa_result = pool.apply_async(soa_shiet, (domain, dns_server, output))
+    ds_result = pool.apply_async(ds_shiet, (domain, dns_server, output))
+
+    response = dict()
+    response['DNSKEY'] = dnskey_result.get()
+    response['SOA'] = soa_result.get()
+    response['DS'] = ds_result.get()
 
     if output:
         pprint.pprint(response)
